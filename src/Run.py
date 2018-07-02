6a1124ada23e3dd211ea99bdda5469aff494aef8
Nicholas Popovic <nicpopovic@me.com>
2018-07-02 20:04:40 +0200

    removed all other classifiers

--- src/Run.py
+++ src/Run.py
@@ -14,15 +14,6 @@ from report.performance_plot import PerformancePlot
 def main():
     data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000,
                       oneHot=True)
-    myStupidClassifier = StupidRecognizer(data.trainingSet,
-                                          data.validationSet,
-                                          data.testSet)
-
-    myLRClassifier = LogisticRegression(data.trainingSet,
-                                        data.validationSet,
-                                        data.testSet,
-                                        learningRate=0.005,
-                                        epochs=30)
 
     myMLPClassifier = MultilayerPerceptron(data.trainingSet,
                                            data.validationSet,
@@ -38,44 +29,25 @@ def main():
     print("=========================")
     print("Training..")
 
-    print("\nStupid Classifier has been training..")
-    myStupidClassifier.train()
-    print("Done..")
-
-    print("\nLogistic Regression has been training..")
-    myLRClassifier.train()
-    print("Done..")
-
     print("\nMLP has been training..")
     myMLPClassifier.train()
     print("Done..")
 
     # Do the recognizer
     # Explicitly specify the test set to be evaluated
-    stupidPred = myStupidClassifier.evaluate()
-    lrPred = myLRClassifier.evaluate()
     mlpPred = myMLPClassifier.evaluate()
 
     # Report the result
     print("=========================")
     evaluator = Evaluator()
 
-    print("Result of the stupid recognizer:")
-    # evaluator.printComparison(data.testSet, stupidPred)
-    evaluator.printAccuracy(data.testSet, stupidPred)
-
-    print("\nResult of the Logistic Regression recognizer:")
-    # evaluator.printComparison(data.testSet, lrPred)
-    evaluator.printAccuracy(data.testSet, lrPred)
-
     print("\nResult of the MLP recognizer:")
     # evaluator.printComparison(data.testSet, lrPred)
     evaluator.printAccuracy(data.testSet, mlpPred)
 
     # Draw
-    plot = PerformancePlot("MLP validation")
-    plot.draw_performance_epoch(myMLPClassifier.performances,
-                                myMLPClassifier.epochs)
+    # plot = PerformancePlot("MLP validation")
+    # plot.draw_performance_epoch(myMLPClassifier.performances, myMLPClassifier.epochs)
 
 
 if __name__ == '__main__':
