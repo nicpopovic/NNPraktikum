5ea6e9045992ead10edd69e302c448a7485daeb4
Nicholas Popovic <nicpopovic@me.com>
2018-07-02 18:02:11 +0200

    Added MLP to Run.py

--- src/Run.py
+++ src/Run.py
@@ -5,6 +5,7 @@ from data.mnist_seven import MNISTSeven
 from model.stupid_recognizer import StupidRecognizer
 from model.perceptron import Perceptron
 from model.logistic_regression import LogisticRegression
+from model.mlp import MultilayerPerceptron
 
 from report.evaluator import Evaluator
 from report.performance_plot import PerformancePlot
@@ -12,27 +13,32 @@ from report.performance_plot import PerformancePlot
 
 def main():
     data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000,
-                                                    oneHot=True)
+                      oneHot=True)
     myStupidClassifier = StupidRecognizer(data.trainingSet,
                                           data.validationSet,
                                           data.testSet)
-    
+
     myPerceptronClassifier = Perceptron(data.trainingSet,
                                         data.validationSet,
                                         data.testSet,
                                         learningRate=0.005,
                                         epochs=30)
-                                        
+
     myLRClassifier = LogisticRegression(data.trainingSet,
                                         data.validationSet,
                                         data.testSet,
                                         learningRate=0.005,
                                         epochs=30)
-                                        
-    
+
+    myMLPClassifier = MultilayerPerceptron(data.trainingSet,
+                                           data.validationSet,
+                                           data.testSet,
+                                           learningRate=0.005,
+                                           epochs=30)
+
     # Report the result #
     print("=========================")
-    evaluator = Evaluator()                                        
+    evaluator = Evaluator()
 
     # Train the classifiers
     print("=========================")
@@ -45,9 +51,13 @@ def main():
     print("\nPerceptron has been training..")
     myPerceptronClassifier.train()
     print("Done..")
-    
+
     print("\nLogistic Regression has been training..")
     myLRClassifier.train()
+    print("Done..")
+
+    print("\nMLP has been training..")
+    myMLPClassifier.train()
     print("Done..")
 
     # Do the recognizer
@@ -55,28 +65,33 @@ def main():
     stupidPred = myStupidClassifier.evaluate()
     perceptronPred = myPerceptronClassifier.evaluate()
     lrPred = myLRClassifier.evaluate()
-    
+    mlpPred = myMLPClassifier.evaluate()
+
     # Report the result
     print("=========================")
     evaluator = Evaluator()
 
     print("Result of the stupid recognizer:")
-    #evaluator.printComparison(data.testSet, stupidPred)
+    # evaluator.printComparison(data.testSet, stupidPred)
     evaluator.printAccuracy(data.testSet, stupidPred)
 
     print("\nResult of the Perceptron recognizer:")
-    #evaluator.printComparison(data.testSet, perceptronPred)
+    # evaluator.printComparison(data.testSet, perceptronPred)
     evaluator.printAccuracy(data.testSet, perceptronPred)
-    
+
     print("\nResult of the Logistic Regression recognizer:")
-    #evaluator.printComparison(data.testSet, lrPred)    
+    # evaluator.printComparison(data.testSet, lrPred)
     evaluator.printAccuracy(data.testSet, lrPred)
-    
+
+    print("\nResult of the MLP recognizer:")
+    # evaluator.printComparison(data.testSet, lrPred)
+    evaluator.printAccuracy(data.testSet, mlpPred)
+
     # Draw
-    plot = PerformancePlot("Logistic Regression validation")
-    plot.draw_performance_epoch(myLRClassifier.performances,
-                                myLRClassifier.epochs)
-    
-    
+    plot = PerformancePlot("MLP validation")
+    plot.draw_performance_epoch(myMLPClassifier.performances,
+                                myMLPClassifier.epochs)
+
+
 if __name__ == '__main__':
     main()
