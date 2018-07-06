fixed backprop problems, added weightDecay, plotted pretty graphs

src/model/logistic_layer.py
@@ -139,16 +139,13 @@ class LogisticLayer():
         # the other is computeOutputLayerDerivative or such.
         return self.deltas
 
-    def updateWeights(self, learningRate):
+    def updateWeights(self, learningRate, weightDecay):
         """
         Update the weights of the layer
         """
 
         # weight updating as gradient descent principle
-        for neuron in range(0, self.nOut):
-            self.weights[:, neuron] -= (learningRate *
-                                        self.deltas[neuron] *
-                                        self.inp)
+        self.weights -= learningRate * self.inp.reshape(-1, 1) * self.deltas.reshape(1, -1) - learningRate * self.weights * weightDecay
 
     def _fire(self, inp):
         return self.activation(np.dot(inp, self.weights))

src/util/activation_functions.py
@@ -73,19 +73,7 @@ class Activation:
     def softmaxPrime(netOutput):
         # compute softmax activation of input
         sm = Activation.softmax(netOutput)
-
-        # initialize Jacobian as diagonal of sm
-        J = np.diag(sm)
-
-        # todo: vectorize
-        for i in range(len(J)):
-            for j in range(len(J)):
-                if i == j:
-                    J[i][j] = sm[i] * (1 - sm[j])
-                else:
-                    J[i][j] = - sm[i] * sm[j]
-
-        return np.sum(J, axis=1)
+        return sm * (1-sm)
         
     @staticmethod
     def getActivation(str):